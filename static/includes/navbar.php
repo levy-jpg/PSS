1b82e2486ae9a7c1762e3c538d6687cd2e254721
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-03-04 13:59:41 +0300

    remove paragraph test

---
+++ static/includes/navbar.php
@@ -0,0 +1,23 @@
+<div class="wrapper">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" id="navA" href="#">PRESENTATION<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" id="navB" href="#">Parking</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" id="navC" href="#">CONTACT</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+    </span>
+  </div>
+</nav>
+</div>
+<h1> TEST</h1>
