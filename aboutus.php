cb70c63ad0d981e4e9e9f91ea2eb07409bd86165
staceybonareri <stacylorrain@gmail.com>
2020-03-05 16:29:32 +0300

    added services

--- aboutus.php
+++ aboutus.php
@@ -3,12 +3,96 @@ $page="About Us";
 include ("head.php");
 ?>
 <div class="container-fluid" id="aus">
-    <div class="row align-items-center min-vh-100 p-4 text-black  rounded">
+    <div class="row align-items-center p-4 text-black  rounded">
         <div class="">
             <h1 class="col-sm-12">ABOUT US</h1>
-            <p>Parking Sytem Solutions is an innovative company that seeks to solve parking solutions within the city.</p>
-                <p>The company started out of a need to solve the long standing crisis around parking management.</p>
-                <p>In conjunction with Kenya Urban Roads Authority (KURA) our partners in this project, we are committed improve parking management within and around country to ensure efficient and easier management of the parking spaces within the country.</p>
+            <p>Parking Sytem Solutions is an innovative company that seeks to solve parking solutions within the city.
+            </p>
+            <p>The company started out of a need to solve the long standing crisis around parking management.</p>
+            <p>In conjunction with Kenya Urban Roads Authority (KURA) our partners in this project, we are committed
+                improve parking management within and around country to ensure efficient and easier management of the
+                parking spaces within the country.</p>
         </div>
     </div>
+</div>
+<div class="conatiner" id="aus">
+    <div class="row align-items-center p-4 text-black  rounded">
+        <div class="div">
+            <h2>SERVICES</h2>
+            <p>Our firm offers efficient and timely solutions to your parking problems. We ensure that through our
+                system we are able to control incoming traffic to your premises at all times. This is achieved through a
+                conducive eco-system of parking-management. </p>
+            <section class="text-center">
+                <button class="btn btn-success">Get Quote</button>
+</section>
+        </div>
+
+    </div>
+</div>
+
+<div class="container">
+    <h1 class="text-center">THE TEAM</h1>
+    <div class="row">
+        <div class="col-md-4 col-sm-6">
+            <div class="card">
+                <img src="static/images/cephas.jpg" alt="Cephas ">
+                <div class="card-body">
+                    <h2>Cephas K. Too</h2>
+                    <p class="small">BACK-END DEVELOPER</p>
+                    <p>A young talented back end developer that ensures that you have efficient parking system</p>
+                    <p>cephaske254@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <div class="card">
+                <img src="static/images/levi.jpg" alt="Levi">
+                <div class="card-body">
+                    <h2>Levi Kariuki</h2>
+                    <p class="title">SCRUM MASTER</p>
+                    <p>Levi is a young talented developer and the point lead in our current project. He ensures flow
+                        of work as we work on the project.</p>
+                    <p>levthigkari@gmail.com</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4 col-sm-6">
+            <div class="card">
+                <img src="static/images/najla.jpg" alt="Najla">
+                <div class="card-body">
+                    <h2>Najla Rashid</h2>
+                    <p class="small">FRONT-END DEVELOPER</p>
+                    <p>She is a talented lady who works on ensuring that everything fits the clients needs and
+                        aesthetic.</p>
+                    <p>rashidnajla770@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <div class="card">
+                <img src="static/images/stacey.jpg" alt="Stacey">
+                <div class="card-body">
+                    <h2>Stacey Bonareri</h2>
+                    <p class="small">FRONT-END DEVELOPER</p>
+                    <p>Stacey is a talented lady who in conjuction with Najla ensure that the clients needs and
+                        aesthetics are met.</p>
+                    <p>stacylorrain@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <div class="card">
+                <img src="static/images/billo.jpg" alt="Diallo">
+                <div class="card-body">
+                    <h2>Billo Dialo</h2>
+                    <p class="small">BACK-END DEVELOPER</p>
+                    <p>Diallo is a young talented developer and assists in the back end development of our
+                        projects.</p>
+                    <p>billodiallonet@gmail.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
