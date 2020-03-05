74b39f2a242efb9fe7addf9d656e63660b68b589
staceybonareri <stacylorrain@gmail.com>
2020-03-05 09:43:57 +0300

    added about us

--- aboutus.php
+++ aboutus.php
@@ -2,9 +2,13 @@
 $page="About Us";
 include ("head.php");
 ?>
-<div class="container">
-    <h1>ABOUT US</h1>
-        <p>Parking Sytem Solutions is an innovative company that seeks to solve parking solutions within the city.</p>
-        <p>The company started out of a need to solve the long standing crisis around parking management.</p>
-        <p>In conjunction with Kenya Urban Roads Authority (KURA) our partners in this project, we are committed improve parking management within and around country to ensure efficient and easier management of the parking spaces within the country.</p>
+<div class="container-fluid" id="aus">
+    <div class="row align-items-center min-vh-100 p-4 text-black  rounded">
+        <div class="">
+            <h1 class="col-sm-12">ABOUT US</h1>
+            <p>Parking Sytem Solutions is an innovative company that seeks to solve parking solutions within the city.</p>
+                <p>The company started out of a need to solve the long standing crisis around parking management.</p>
+                <p>In conjunction with Kenya Urban Roads Authority (KURA) our partners in this project, we are committed improve parking management within and around country to ensure efficient and easier management of the parking spaces within the country.</p>
+        </div>
+    </div>
 </div>
